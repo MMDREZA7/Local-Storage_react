49e2a85496d2f2fb52cca05171ca189dd693a787
MMDREZA <mheidari831003.game@gmail.com>
2024-06-01 12:04:57 +0330

    Started

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todos from "./compenents/todos/Todos";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="background-main">
+      <div className="backGround">
+        <Todos />
+      </div>
+    </div>
+  );
+}
+
+export default App;
